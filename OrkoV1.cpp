b97aac1ba6357fe77c35aa47f06715d26dad9323
FlexSec <FlexInfoSec@gmail.com>
2017-07-05 21:26:26 -0700

    V1
    Moved Offsets to seperate Header File

--- OrkoV1.cpp
+++ OrkoV1.cpp
@@ -1,28 +1,13 @@
-// OrkoV1.cpp : Defines the entry point for the console application.
-//
-
+#pragma once
 #include "stdafx.h"
 #include "MMC.h"
 #include <iostream>
 #include <Windows.h>
 #include <TlHelp32.h>
+#include "Offsets.h"
 
 
 MMC* Mem;
-
-// Offsets
-
-#define m_dwLocalPlayer 0x00A9E8E4
-#define  m_iCrossHairID 0x0000AA70
-#define m_iTeamNum 0x000000F0
-#define m_dwEntityList 0x04AC0CA4
-#define m_bDormant 0x000000E9
-#define m_dwForceAttack 0x02F00DF4
-#define m_dwForceJump 0x04F5782C
-#define m_dwForceLeft 0x02F00DB8
-#define  m_dwForceRight 0x02F00DC4
-#define  m_fFlags 0x00000100
-#define m_FL_ONGROUND (1 << 0)
 
 bool TriggerOn = false;
 bool HopOn = false;
@@ -42,12 +27,9 @@ void Trigger()
 
 		if (!TriggerOn) continue;
 
-
 		DWORD LocalPlayer_Base = Mem->Read<DWORD>(Mem->ClientDLL_Base + m_dwLocalPlayer);
 		int LocalPlayer_inCross = Mem->Read<int>(LocalPlayer_Base + m_iCrossHairID);
 		int LocalPlayer_Team = Mem->Read<int>(LocalPlayer_Base + m_iTeamNum);
-
-
 		DWORD Trigger_EntityBase = Mem->Read<DWORD>(Mem->ClientDLL_Base + m_dwEntityList + ((LocalPlayer_inCross - 1) * 0x10));
 		int Trigger_EntityTeam = Mem->Read<int>(Trigger_EntityBase + m_iTeamNum);
 		bool Trigger_EntityDormant = Mem->Read<bool>(Trigger_EntityBase + m_bDormant);
@@ -67,10 +49,8 @@ void Trigger()
 
 void EasterJump()
 {
-
 	while (true)
 	{
-
 		if (GetAsyncKeyState(VK_SHIFT))		//SET KEY HERE
 		{
 			HopOn = !HopOn;
@@ -81,7 +61,6 @@ void EasterJump()
 
 		if (!HopOn) continue;
 
-		
 		DWORD LocalPlayer_Base = Mem->Read<DWORD>(Mem->ClientDLL_Base + m_dwLocalPlayer);
 		DWORD ForceJump = Mem->Read<DWORD>(Mem->ClientDLL_Base + m_dwForceJump);
 		int flags = Mem->Read<DWORD>(LocalPlayer_Base + m_fFlags); 
