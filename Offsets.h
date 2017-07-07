a68366ea1867969ce5964ce32405d6d1a17478de
FlexSec <FlexInfoSec@gmail.com>
2017-07-06 20:32:33 -0700

    Offsets Header
    Added Header file with some memory offsets

---
+++ Offsets.h
@@ -0,0 +1,14 @@
+#pragma once
+// Offsets
+
+#define m_dwLocalPlayer 0x00A9E8E4
+#define  m_iCrossHairID 0x0000AA70
+#define m_iTeamNum 0x000000F0
+#define m_dwEntityList 0x04AC0CA4
+#define m_bDormant 0x000000E9
+#define m_dwForceAttack 0x02F00DF4
+#define m_dwForceJump 0x04F5782C
+#define m_dwForceLeft 0x02F00DB8
+#define  m_dwForceRight 0x02F00DC4
+#define  m_fFlags 0x00000100
+#define m_FL_ONGROUND (1 << 0)
